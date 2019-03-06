ca46c70a7e9554234dee2fb0cac876919ecf17ea
Sandip Gautam <rockersandip96@gmail.com>
2019-03-06 19:08:39 +0200

    Initial Commit

---
+++ FlightBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlightBoard
+//
+//  Created by iosdev on 06/03/2019.
+//  Copyright © 2019 San Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
